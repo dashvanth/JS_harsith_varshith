7a5c33f162e84193d1b402979e0c59838755907c
Dashvanth <dashvanthrajhc@gmail.com>
2023-10-10 23:08:04 +0530

    Delete Part_02/02_DOM_Tutorial directory

--- Part_02/02_DOM_Tutorial/08_innerHTML.js
+++
@@ -1,6 +0,0 @@
-// innerHTML 
-
-const headLine = document.querySelector(".headline");
-console.log(headLine);
-headLine.innerHTML = "<h1>HEadline was changed</h1>";
-headLine.innerHTML += `<button class="btn">done</button>`;
