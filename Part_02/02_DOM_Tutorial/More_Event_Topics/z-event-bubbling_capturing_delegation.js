c59de3d445ffb32a67d0481e3566d630ccf810ab
Dashvanth <dashvanthrajhc@gmail.com>
2023-10-10 23:17:48 +0530

    Update event-bubbling capturing delegation

--- Part_02/02_DOM_Tutorial/More_Event_Topics/z-event-bubbling_capturing_delegation.js
+++ Part_02/02_DOM_Tutorial/More_Event_Topics/z-event-bubbling_capturing_delegation.js
@@ -3,43 +3,54 @@
 // console.log("hello world");
 
 const grandParent = document.querySelector(".grandparent");
-// const parent = document.querySelector(".parent");
-// const child = document.querySelector(".child");
-// const body = document.body;
-// child.addEventListener("click", ()=>{
-//     console.log("bubble Child");
-// })
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+const body = document.body;
+child.addEventListener("click", ()=>{
+    console.log("bubble Child");
+})
 
-// grandParent.addEventListener("click", ()=>{
-//     console.log("bubble Grand Parent");
-// })
+grandParent.addEventListener("click", ()=>{
+    console.log("bubble Grand Parent");
+})
 
-// parent.addEventListener("click", ()=>{
-//     console.log("bubble parent");
-// })
-// body.addEventListener("click", ()=>{
-//     console.log("bubble body");
-// })
+parent.addEventListener("click", ()=>{
+    console.log("bubble parent");
+})
+body.addEventListener("click", ()=>{
+    console.log("bubble body");
+})
 
 // =============================================
 // event capturing 
 
-// child.addEventListener("click", ()=>{
-//     console.log("Capture Child");
-// }, true)
+child.addEventListener("click", ()=>{
+    console.log("Capture Child");
+}, true)
 
-// grandParent.addEventListener("click", ()=>{
-//     console.log("Capture Grand Parent");
-// }, true)
+grandParent.addEventListener("click", ()=>{
+    console.log("Capture Grand Parent");
+}, true)
 
-// parent.addEventListener("click", ()=>{
-//     console.log("Capture parent");
-// }, true)
-// body.addEventListener("click", ()=>{
-//     console.log(("Capture body"));
-// })
+parent.addEventListener("click", ()=>{
+    console.log("Capture parent");
+}, true)
+body.addEventListener("click", ()=>{
+    console.log(("Capture body"));
+})
 
 // event delegation -> we use (e) to control bubbling and capturing of event 
+child.addEventListener("click", (e)=>{
+    console.log(e.target);
+}, true)
+
 grandParent.addEventListener("click", (e)=>{
-    console.log(e);
+    console.log(e.target);
+}, true)
+
+parent.addEventListener("click", (e)=>{
+    console.log(e.target);
+}, true)
+body.addEventListener("click", (e)=>{
+    console.log(e.target);
 })
